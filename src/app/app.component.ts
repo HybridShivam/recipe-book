c591674e9c830962f32bb98c445235209ef37284
Shivam Baghel <shivamunlimited@gmail.com>
2020-04-06 10:02:34 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`h3{
+    color: chartreuse;
+  }
+  `]
 })
 export class AppComponent {
   title = 'hybrid-angular';
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+  serverID = 10;
+  serverStatus = 'Offline';
+  serverName = '';
+
+  /*
+   Lets create a method
+  */
+  getServerStatus() {
+    return this.serverName;
+  }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
+
+  getBackgroundColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+}
