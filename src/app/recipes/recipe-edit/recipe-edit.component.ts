f54ade5c40c5426dd55d990100c9c3de561dc9c4
Shivam Baghel <shivamunlimited@gmail.com>
2020-04-13 20:51:11 +0530

    Update recipe-edit.component.ts

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -19,7 +19,6 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = params['id'];
         this.editMode = params['id'] != null;
-        // console.log(this.editMode);
       }
     );
   }
