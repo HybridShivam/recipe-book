0852690a0a8f8f57ddf7125681bc7c8dc4a0d7c2
Shivam Baghel <shivamunlimited@gmail.com>
2022-08-30 12:34:32 +0530

    renamed package

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/hybrid-angular'),
+      dir: require('path').join(__dirname, './coverage/recipe-book'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
